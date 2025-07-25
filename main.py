3a66f4625517627cdc9a3d2b4f3754ebde30b8d8
Tyrnpuu <reivotyrp@gmail.com>
2025-07-25 17:12:30 +0300

    add system argument functionality

--- main.py
+++ main.py
@@ -1,11 +1,17 @@
 from stats import get_num_words, get_num_characters, sort_characters
+import sys
 
 def get_book_text(path):
     with open(path) as f:
         return f.read()
 
 def main():
-    rel_path = "./books/frankenstein.txt"
+    try:
+        rel_path = sys.argv[1]
+    except IndexError:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+        
     text = get_book_text(rel_path)
     num_words = get_num_words(text)
     num_characters = get_num_characters(text)
